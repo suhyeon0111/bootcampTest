a0f3f2c2bbecd170823673b73c8ee5b2dc4d2504
suhyeon0111 <soo4833@naver.com>
2025-06-02 21:08:32 +0900

    feat: listItem 틀 작성

--- my-app/src/components/List.tsx
+++ my-app/src/components/List.tsx
@@ -1,48 +1,53 @@
-// import React from "react";
+import React from "react";
+import { TList } from "@/app/page";
 
-// export default function List() {
+interface ListProps {
+    allLists: TList[];
+}
 
-//     return (
-//         <>
-//                 {/* todo리스트 관리 */}
-//         <img src="/img/todo.svg" alt="todo" />
-//         {todos.length === 0 ? (
-//           // todo리스트 없을 경우 
-//           <div>
-//             <img src="/img/Logo_todo.svg" alt="Logo_todo" />
-//             <p>
-//               할 일이 없어요.
-//               <br />
-//               TODO를 새롭게 추가해주세요!
-//             </p>
-//           </div>
-//         ) : (
-//           // 미완료 리스트 관리  
-//           <div>
-//             {todos.filter((item) => !item.iscompleted)
-//               .map((item) => (
-//                 <p key={item.id}>{item.name}</p>
-//               ))}
-//           </div>
-//         )}
 
-//         {/* done리스트 관리 */}
-//         <img src="/img/done.svg" />
-//         {dones.length === 0 ? (
-//           // 완료 리스트 없을때 
-//           <div>
-//             <img src="/img/Logo_done.svg" alt="Logo_done" />
-//             <p>
-//               아직 다 한 일이 없어요
-//               <br />
-//               해야 할 일을 체크해보세요!
-//             </p>
-//           </div>
-//         ) : (
-//           <div>{dones.map((item) => (
-//             <p key={item.id}>{item.name}</p>
-//           ))}</div>
-//         )}  
-//         </>
-//     )
-// }
+export default function List({ allLists }: ListProps) {
+    return (
+        <>
+            {/* todo리스트 관리 */}
+            <img src="/img/todo.svg" alt="todo" />
+            {todos.length === 0 ? (
+                // todo리스트 없을 경우 
+                <div>
+                    <img src="/img/Logo_todo.svg" alt="Logo_todo" />
+                    <p>
+                        할 일이 없어요.
+                        <br />
+                        TODO를 새롭게 추가해주세요!
+                    </p>
+                </div>
+            ) : (
+                // 미완료 리스트 관리  
+                <div>
+                    {todos.filter((item) => !item.iscompleted)
+                        .map((item) => (
+                            <p key={item.id}>{item.name}</p>
+                        ))}
+                </div>
+            )}
+
+            {/* done리스트 관리 */}
+            <img src="/img/done.svg" />
+            {dones.length === 0 ? (
+                // 완료 리스트 없을때 
+                <div>
+                    <img src="/img/Logo_done.svg" alt="Logo_done" />
+                    <p>
+                        아직 다 한 일이 없어요
+                        <br />
+                        해야 할 일을 체크해보세요!
+                    </p>
+                </div>
+            ) : (
+                <div>{dones.map((item) => (
+                    <p key={item.id}>{item.name}</p>
+                ))}</div>
+            )}
+        </>
+    )
+}
--- my-app/src/components/ListItem.tsx
+++ my-app/src/components/ListItem.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { TList } from "@/app/page";
 
-export default function ListItem() {
+
+export default function ListItem(TItem: TList) {
     return (
         <>
+            <p>{TItem.name}</p>
         </>
     )
 }
