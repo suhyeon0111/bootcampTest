0e042e5e7d1adaef4fa861a78c8490a9812df4be
suhyeon0111 <soo4833@naver.com>
2025-06-08 01:23:46 +0900

    fix: header doit 이미지 위치 변경

--- my-app/src/components/Header.tsx
+++ my-app/src/components/Header.tsx
@@ -1,8 +1,10 @@
 export default function Header() {
     return (
         <header className="absolute lg:w-full bg-white outline-2 outline-slate-200">
-            <img src="img/Size=Large.svg" alt="logo" className="hidden md:block" />
-            <img src="img/Size=Small.svg" alt="mobile_logo" className="block sm:hidden" />
+            <div className="max-w-5xl mx-auto py-1 flex items-center">
+                <img src="img/Size=Large.svg" alt="logo" className="hidden md:block " />
+                <img src="img/Size=Small.svg" alt="mobile_logo" className="block sm:hidden" />
+            </div>
         </header>
     )
 }
