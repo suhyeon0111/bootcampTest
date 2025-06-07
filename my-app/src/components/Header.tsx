f87b3dfbb74f5fbaeae9fecd3467b8bc833b7d21
suhyeon0111 <soo4833@naver.com>
2025-06-08 01:16:46 +0900

    fix: header css 수정

--- my-app/src/components/Header.tsx
+++ my-app/src/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <header className="lg:w-full outline-2 outline-[#E2E8F0]">
+        <header className="absolute lg:w-full bg-white outline-2 outline-slate-200">
             <img src="img/Size=Large.svg" alt="logo" className="hidden md:block" />
             <img src="img/Size=Small.svg" alt="mobile_logo" className="block sm:hidden" />
         </header>
